b0f4600f87b7e76e0d6c3052398d13d404c8605a
Udaya Ranga <udaya.ranga@arm.com>
2015-09-29 16:40:45 +0100

    CH-6714: Pass 1st test in word-count
    Change-Id: Icd970f84a36087e15d70ca16f82eaa0b22373342

--- cpp/word-count/word_count.cpp
+++ cpp/word-count/word_count.cpp
@@ -3,7 +3,17 @@
 namespace word_count	{
 	const dict_t words(const std::string& str)	{
 		dict_t wc;
-		wc[str]	= 1;
+		//wc[str]	= 1;
+        auto word       = std::string("");
+        char space_ch   = ' ';
+        auto begin_pos  = 0;
+        auto pos_space  = str.find(space_ch);
+        while(pos_space != std::string::npos)   {
+            word        = str.substr(begin_pos, pos_space - begin_pos);
+            ++wc[word];
+            begin_pos   = pos_space + 1;// Even if this results in npos, it is OK
+            pos_space   = str.find(space_ch, pos_space);// This result guarantees that begin_pos remain valid at all times
+        }
 		return wc;
 	}
 }
