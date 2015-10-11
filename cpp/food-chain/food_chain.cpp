b9c771bc8563c4167dcc31a959834d6d036af1a9
Udaya Ranga <uday87j@gmail.com>
2015-10-11 12:25:25 +0100

    CH-24322: Refaactor food-chain

--- cpp/food-chain/food_chain.cpp
+++ cpp/food-chain/food_chain.cpp
@@ -1,10 +1,13 @@
 #include "food_chain.h"
 
 #include <cassert>
+#include <vector>
 
 using namespace std;
 
 namespace food_chain	{
+
+    std::vector<std::string> lives{ "fly", "spider", "bird", "cat", "dog", "goat", "cow", "horse"};
 	
 	std::string start_str(const std::string& life)	{
 		return "I know an old lady who swallowed a " + life + ".\n";
@@ -14,7 +17,7 @@ namespace food_chain	{
 		return "I don't know why she swallowed the fly. Perhaps she'll die.\n";
 	}
 
-	std::string spider_str(const std::string& prefix)	{
+	std::string spider_str(const std::string& prefix = "It")	{
 		return prefix + " wriggled and jiggled and tickled inside her.\n";
 	}
 
@@ -46,34 +49,61 @@ namespace food_chain	{
 		return "She swallowed the " + predator + " to catch the " + prey;
 	}
 
+    std::string lyrics(verse_traits::uint_t i)    {
+        switch(i)   {
+            case 1:
+            	return fly_str();
+                break;
+            case 2:
+            	return spider_str();
+                break;
+            case 3:
+            	return bird_str();
+                break;
+            case 4:
+            	return cat_str();
+                break;
+            case 5:
+            	return dog_str();
+                break;
+            case 6:
+            	return goat_str();
+                break;
+            case 7:
+            	return cow_str();
+                break;
+            case 8:
+            	return horse_str();
+                break;
+            default:
+                assert(0);
+                break;
+        }
+        return "";
+    }
+
 	std::string connect_verses(verse_traits::uint_t v)	{
 		switch(v)	{
 			case 2:
-				return swallow_str("spider", "fly") + ".\n" + fly_str();
+				return swallow_str(lives[v-1], lives[v-2]) + ".\n" + fly_str();
 				break;
 
 			case 3:
-				return  swallow_str("bird", "spider") + spider_str(" that")
+				return  swallow_str(lives[v-1], lives[v-2]) + spider_str(" that")
 						+ connect_verses(v - 1);
 				break;
 
 			case 4:
-				return swallow_str("cat", "bird") + ".\n"
-						+ connect_verses(v - 1);
-				break;
+			    ;//Fall through
 
             case 5:
-                return swallow_str("dog", "cat") + ".\n"
-						+ connect_verses(v - 1);
-                break;
+                ;//Fall through
 
             case 6:
-                return swallow_str("goat", "dog") + ".\n"
-						+ connect_verses(v - 1);
-                break;
+                ;//Fall through
 
             case 7:
-                return swallow_str("cow", "goat") + ".\n"
+                return swallow_str(lives[v-1], lives[v-2]) + ".\n"
 						+ connect_verses(v - 1);
                 break;
 
@@ -98,41 +128,33 @@ namespace food_chain	{
 	std::string verse(verse_traits::uint_t v)	{
 		switch(v)	{
 			case 1:
-				return start_str("fly") + fly_str();
+				return start_str(lives[v - 1]) + lyrics(v);
 				break;
 
 			case 2:
-				return start_str("spider") + spider_str("It") + connect_verses(v);
+				return start_str(lives[v - 1]) + spider_str("It") + connect_verses(v);
 				break;
 
 			case 3:
-				return start_str("bird") + bird_str()
-					+ connect_verses(v);
-				break;
+				;//Fall through
 
 			case 4:
-				return start_str("cat") + cat_str()
+				;//Fall through
+
+            case 5:
+            	;//Fall through
+
+            case 6:
+            	;//Fall through
+
+			case 7:
+				return start_str(lives[v - 1]) + lyrics(v)
 					+ connect_verses(v);
 				break;
 
-            case 5:
-                return start_str("dog") + dog_str()
-					+ connect_verses(v);
-                break;
-
-            case 6:
-                return start_str("goat") + goat_str()
-					+ connect_verses(v);
-                break;
-
-            case 7:
-                return start_str("cow") + cow_str()
-					+ connect_verses(v);
-                break;
-
-                case 8:
-                return start_str("horse") + horse_str();
-                break;
+			case 8:
+				return start_str("horse") + lyrics(v);
+				break;
 
 			default:
 				assert(0);
