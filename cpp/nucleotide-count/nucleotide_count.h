5a349f137758d59cf247d62e1749a20c75138660
Udaya Ranga <udaya.ranga@arm.com>
2015-10-29 15:40:09 +0000

    CH-1986: Refactor nucleotide: Remove m_str
    Change-Id: Ieebca7a205f9a407ea23a6eac291adc5386fba52

--- cpp/nucleotide-count/nucleotide_count.h
+++ cpp/nucleotide-count/nucleotide_count.h
@@ -11,29 +11,23 @@ namespace dna   {
 
     typedef std::map<char, int> ci_map_t;
 
-    void print_map(const ci_map_t& g)    {
-        for(auto& m : g)    {
-            std::cout << m.first << ":\t" << m.second << std::endl;
-        }
-    }
-
     class counter   {
         public:
-            counter(std::string str = "")
-                : m_str(str)    {
+            counter(std::string str = "")   {
                     m_cimap['A']    = 0;
                     m_cimap['T']    = 0;
                     m_cimap['C']    = 0;
                     m_cimap['G']    = 0;
-                    for(auto& c : m_str)    {
-                        if(valid_char(c))   {
-                            ++m_cimap[c];
-                        }
-                        else    {
-                            throw std::invalid_argument("Arg is not a DNA strand");//Make this work!
-                        }
-                    }
-                    //print_map(m_cimap);
+                    std::for_each(std::begin(str), std::end(str),
+                        [this](const char& c)    {
+                            if(valid_char(c))   {
+                                ++m_cimap[c];
+                            }
+                            else    {
+                                throw std::invalid_argument("Arg is not a DNA strand");
+                            }
+
+                        });
                 }
             ci_map_t nucleotide_counts() const {
                 return m_cimap;
@@ -45,7 +39,6 @@ namespace dna   {
             bool valid_char(const char c)   {
                 return ((c == 'A') || (c == 'C') || (c == 'T') || (c == 'G'));                    
             }
-            std::string m_str;
             ci_map_t m_cimap;
     };
 }
